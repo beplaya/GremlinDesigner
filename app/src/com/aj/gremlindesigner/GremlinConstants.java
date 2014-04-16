cef7d57eb9d8fdfd9e3cc1fc0ff84627ac397426
Grant <beplaya@gmail.com>
2014-04-16 17:29:28 -0400

    icon, all have at least 2 options

--- app/src/com/aj/gremlindesigner/GremlinConstants.java
+++ app/src/com/aj/gremlindesigner/GremlinConstants.java
@@ -5,8 +5,8 @@ import com.aj.gremlindesigner.R.drawable;
 public class GremlinConstants {
 	public static final String PREF_KEY_GREMLIN = "GremlinBuilder.PREFENCE_KEY";
 	public static final int[] HEAD_OPTIONS = new int[] { drawable.head_001, drawable.head_002, drawable.head_003 };
-	public static final int[] BODY_OPTIONS = new int[] { drawable.body_001 };
-	public static final int[] HAND_OPTIONS = new int[] { drawable.hands_001 };
-	public static final int[] FEET_OPTIONS = new int[] { drawable.feet_001 };
-	public static final int[] EYE_OPTIONS = new int[] { drawable.eyes_001 };
+	public static final int[] BODY_OPTIONS = new int[] { drawable.body_001, drawable.body_002 };
+	public static final int[] HAND_OPTIONS = new int[] { drawable.hands_001, drawable.hands_002 };
+	public static final int[] FEET_OPTIONS = new int[] { drawable.feet_001, drawable.feet_002 };
+	public static final int[] EYE_OPTIONS = new int[] { drawable.eyes_001, drawable.eyes_002 };
 }
